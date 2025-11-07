8b313292d686ad95158495dad358008b33154464
viih1234 <vitoria.cordeiro.neves@escola.pr.gov.br>
2025-11-07 08:39:51 -0300

    carousel.js

---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+
+//Percorrer todos os botoes controladores
+btns.forEach(function(btn) {
+    btn.addEventListener('click' , function()) {
+
+
+        //Remover class 'ativo ' dos outros botoes 
+        btns.forEach(function(btnRemoveclass) {
+            btnRemoveclass.classList.remove('ListaDeArtigos-slider-item-ativo')
+        })
+
+        this .classList.add('ListaDeArtigo-slider-item--ativo')
+    }
+}
